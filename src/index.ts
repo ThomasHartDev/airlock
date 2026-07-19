47c58833085620bb86d23bc89e7917ba8fb472b2
Thomas Hart <thomashartdev@gmail.com>
2026-07-19 16:21:37 -0600

    feat: add run(code, opts) sandbox with zero-credential invariant (#2)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 export { runVerified } from "./run.js";
 export { isVerified } from "./contract.js";
+export {
+  run,
+  probeAmbientAuthority,
+  ZeroCredentialViolation,
+  DENIED_AMBIENT_NAMES,
+} from "./sandbox.js";
+export type { SandboxRunOptions } from "./sandbox.js";
 export type {
   Assertion,
   RunResult,
